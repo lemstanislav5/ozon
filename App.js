12dfa02d7eee873a47e7b8386fa4543f080ebdd8
lemstanislav5 <lemstanislav5@gmail.com>
2022-05-06 08:45:23 +0300

    06.05.22

--- App.js
+++ App.js
@@ -1,9 +1,10 @@
 import { StatusBar } from 'expo-status-bar';
 import React, { StyleSheet, Text, View, Image } from 'react-native';
 import { NativeBaseProvider, Box, Button, Center, ScrollView, Heading } from "native-base";
-import { products } from './data'
 import { Header } from './components/Header/Header';
 import { Footer } from './components/Footer/Footer'
+import { products } from './data'
+import SortableGridView from 'react-native-sortable-gridview'
 
 export default function App() {
   return (
@@ -11,33 +12,65 @@ export default function App() {
       <StatusBar style="auto"/>
       <View style={{flex: 1}}>
         <Header/>
-        <ScrollView>
-          <Box style={{marginTop: 10}} p={5}>
-            <Heading sixe='lg' color='#111'>
-              Успей купить!
-            </Heading>
-            <View mt={10} flexErap='wrap' justifyContent='space-between'>
-              {products.map((product, i) => (
-                <Box key={`product ${i}`} style={{width: '30%'}}>
-                  <Image style={{ width: 200, height: 200, flex: 1, position: 'relative' }}
-                    size='md'
-                    resizeMode="cover"
-                    source={{
-                      uri: 'https://el-koleso.ru/image/cache/catalog/tovary/velosipedy/150/150-12-800x600.jpg',
-                      position: 'relative'
-                    }}
-                    alt={product.name}
-                  />
-                  <Text>
-                    {product.price.toLocaleString('ru-Ru', {
-                      style: 'currency',
-                      currency: 'RUB'
-                    })}
-                  </Text>
-                </Box>
-              ))}
-            </View>
-          </Box>
+        <ScrollView columns={3} spacing={5}>
+            <Box style={{marginTop: 10}} p={5}>
+              <Heading sixe='lg' color='#111'>
+                Успей купить!
+              </Heading>
+              <View mt={10} flexErap='wrap' justifyContent='space-between'>
+              <SortableGridView
+                data={products}
+                onDragStart={() => {
+                  console.log('Default onDragStart');
+                }}
+                onDragRelease={(data) => {
+                  console.log('Default onDragRelease', data);
+                }}
+                renderItem={(product, i) => {
+                  return (
+                    <Box key={`product ${i}`} style={{height: 200, alignItems: 'center', padding: 5, marginBottom: 10}}>
+                    <Image style={{width: 100, height: 100, flex: 1, position: 'relative' }}
+                      size='md'
+                      resizeMode="cover"
+                      source={{
+                        uri: product.image,
+                        position: 'relative'
+                      }}
+                      alt={product.name}
+                    />
+                    {product.type !== null
+                      ? <Text style={{ flex: 1, position: 'absolute', textAlign: 'center', bottom: 20, fontWeight: 700, color: '#fc1f1f'}}>
+                          {product.type}
+                        </Text>
+                      :<></>
+            
+                    }
+                    <Text style={{  flex: 1, position: 'relative', textAlign: 'center', margin: 5}}>
+                      {product.name}
+                    </Text>
+                    <Text  style={{  flex: 1, position: 'relative', textAlign: 'center', margin: 5}}>
+                      {product.price.toLocaleString('ru-Ru', {
+                        style: 'currency',
+                        currency: 'RUB'
+                      })}
+                    </Text>
+                    <Button 
+                      style={{ 
+                        width: 150, 
+                        height: 100, 
+                        flex: 1, 
+                        position: 'relative', 
+                        textAlign: 'center', 
+                        margin: 5,
+                        backgroundColor: '#333'
+                      }}
+                      onPress={() => console.log("hello world")}>В корзину</Button>
+                  </Box>
+                  )
+                }}
+              />
+              </View>
+            </Box>
         </ScrollView>
         <Footer/>
       </View>
--- data/index.ts
+++ data/index.ts
@@ -3,6 +3,7 @@ type Product = {
   price: number
   premiun: number | null
   type: string | null
+  image: string
 }
 
 export const products: Array <Product> = [
@@ -10,42 +11,42 @@ export const products: Array <Product> = [
     name: 'Гоночный велосипед',
     price: 10000,
     premiun: 7000,
-    type: 'Хит'
+    type: 'Хит',
+    image: 'https://el-koleso.ru/image/cache/catalog/tovary/velosipedy/150/150-12-800x600.jpg'
   },
   {
     name: 'Конструктор Lego',
     price: 2000,
     premiun: 1100,
-    type: null
+    type: null,
+    image: 'https://www.lego.com/cdn/cs/set/assets/blt71f2b3a3251f7184/71363.jpg'
   },
   {
     name: 'iPhone XS',
     price: 70000,
     premiun: 67000,
-    type: 'Редкость'
+    type: 'Редкость',
+    image: 'https://appleinsider.ru/wp-content/uploads/2022/01/iphone_xs_main_pict-740x473.jpg'
   },
   {
     name: 'Корм для кошек',
     price: 1000,
     premiun: null,
     type: null,
+    image: 'https://www.bethowen.ru/upload/iblock/55b/55bad7f922c8712744554d5e43e165d2.jpg'
   },
   {
     name: 'Беспроводные наушники',
     price: 5000,
     premiun: 4900,
-    type: 'Хит'
+    type: 'Хит',
+    image: 'https://mi-shop.com/upload/resize_cache/iblock/e84/445_473_2/e84621717359a9dad1efa0b7a586d06c.jpg'
   },
   {
     name: 'Фен для волос',
     price: 10000,
     premiun: 9000,
-    type: '30% скидка'
-  },
-  {
-    name: 'Гоночный велосипед',
-    price: 10000,
-    premiun: 7000,
-    type: 'Хит'
+    type: '30% скидка',
+    image: 'https://groomershop.ru/wp-content/uploads/2019/10/Dewal-Spectrum-03-110-Red.jpg'
   }
 ]
